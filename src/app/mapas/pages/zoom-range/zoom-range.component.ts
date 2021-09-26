8d1d80fae2c65404b0abcb1c602cffc71724ea0b
Alejandro Arguello <daw19.2021@gmail.com>
2021-09-26 14:15:21 +0200

    implements controlling the map object(DOM) -ZoomIn and ZoomOut- (lesson 309 course angular)

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import * as mapboxgl from "mapbox-gl";
 
 @Component({
@@ -23,17 +23,30 @@ import * as mapboxgl from "mapbox-gl";
     `
   ]
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit  {
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-    let map = new mapboxgl.Map({
-      container: 'mapa',
+  //Usamos el ngAfterViewInit porque la referencia que cogemos del DOM con el ViewChild
+  // no se llega a cargar en el ngOnInit por lo tanto nos dara undefined este elemento
+  ngAfterViewInit() : void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-2.4574804196745426,42.464012977763495],
       zoom: 17
     });
   }
+
+  zoomOut(): void {
+    this.mapa.zoomOut();
+  }
+
+  zoomIn(): void {
+    this.mapa.zoomIn();
+  }
+
 
 }
