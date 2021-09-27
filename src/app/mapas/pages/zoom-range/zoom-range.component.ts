2da863e24319075257654de4dbace02cc06fd0d1
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-09-27 17:51:14 +0200

    implements controller level of zoom (lesson 310 course angular)

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -26,27 +26,29 @@ import * as mapboxgl from "mapbox-gl";
 export class ZoomRangeComponent implements AfterViewInit  {
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
+  zoomLevel: number = 16;
 
   constructor() {}
 
   //Usamos el ngAfterViewInit porque la referencia que cogemos del DOM con el ViewChild
-  // no se llega a cargar en el ngOnInit por lo tanto nos dara undefined este elemento
+  //no se llega a cargar en el ngOnInit por lo tanto nos dara undefined este elemento
   ngAfterViewInit() : void {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-2.4574804196745426,42.464012977763495],
-      zoom: 17
+      zoom: this.zoomLevel
     });
   }
 
   zoomOut(): void {
     this.mapa.zoomOut();
+    this.zoomLevel = this.mapa.getZoom();
   }
 
   zoomIn(): void {
     this.mapa.zoomIn();
+    this.zoomLevel = this.mapa.getZoom();
   }
-
 
 }
