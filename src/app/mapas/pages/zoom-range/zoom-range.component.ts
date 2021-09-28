1c617fe076145ba3f5859198021fa58f784574cb
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-09-28 18:30:22 +0200

    get the central coordinates of the map (lesson 313 course angular)

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
 import * as mapboxgl from "mapbox-gl";
 
 @Component({
@@ -18,17 +18,25 @@ import * as mapboxgl from "mapbox-gl";
         padding:10px;
         position:fixed;
         z-index:999;
-        width: 20rem;
+        width: 25rem;
       }
     `
   ]
 })
-export class ZoomRangeComponent implements AfterViewInit  {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
   zoomLevel: number = 16;
+  center: [number,number] = [-2.4574804196745426,42.464012977763495];
 
   constructor() {}
+
+  ngOnDestroy(): void {
+    // Destruimos los listener ya que podrían quedarse activos
+    this.mapa.off('zoom', () => {} )
+    this.mapa.off('zoomend', () => {} )
+    this.mapa.off('move', () => {} )
+  }
 
   //Usamos el ngAfterViewInit porque la referencia que cogemos del DOM con el ViewChild
   //no se llega a cargar en el ngOnInit por lo tanto nos dara undefined este elemento
@@ -36,16 +44,27 @@ export class ZoomRangeComponent implements AfterViewInit  {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-2.4574804196745426,42.464012977763495],
+      center: this.center,
       zoom: this.zoomLevel
     });
 
-    this.mapa.on('zoom', () => this.zoomLevel =this.mapa.getZoom());
+    //Escuchamos el evento zoom de la librería y le asignamos el valor a nuestra variable zoomLevel
+    this.mapa.on('zoom', () => this.zoomLevel = this.mapa.getZoom());
+
+    //Limitación final del zoom al valor 19 de zoom
     this.mapa.on('zoomend', () => {
       if (this.mapa.getZoom() > 19){
         this.mapa.zoomTo(19);
       }
     });
+
+    //Movimiento del mapa (Latitud y Longitud)
+    this.mapa.on('move', (event) =>{
+      const target = event.target;
+      const { lng, lat } = target.getCenter();
+      this.center = [ lng , lat ];
+    });
+
   }
 
   zoomOut(): void {
@@ -59,4 +78,5 @@ export class ZoomRangeComponent implements AfterViewInit  {
   zoomCambio(value: string): void {
     this.mapa.zoomTo(Number(value))
   }
+
 }
