904ae54baa3791d527d168e4eb912d17354e9ab8
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-10-01 14:19:38 +0200

    implements markers of dynamically mood (lesson 317 course angular)

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -9,6 +9,15 @@ import * as mapboxgl from "mapbox-gl";
       width: 100%;
       height: 100%;
     }
+    .list-group{
+      position: fixed;
+      top: 20px;
+      right: 20px;
+      z-index: 99;
+    }
+    li{
+      cursor: pointer;
+    }
     `
   ]
 })
@@ -34,9 +43,23 @@ export class MarcadoresComponent implements AfterViewInit {
     // const markerHtml: HTMLElement = document.createElement('div');
     // markerHtml.innerHTML = 'Hola mundo';
 
-    new mapboxgl.Marker()
-      .setLngLat(this.center)
-      .addTo(this.mapa);
+    // new mapboxgl.Marker()
+    //   .setLngLat(this.center)
+    //   .addTo(this.mapa);
+  }
+  agregarMarcador(){
+
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+
+    const nuevoMarcador = new mapboxgl.Marker({
+      draggable: true,
+      color: color,
+    })
+      .setLngLat( this.center )
+      .addTo( this.mapa );
   }
 
+  irMarcador(){
+
+  }
 }
