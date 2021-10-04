c737fd0b356c06d0309711dec8023fbec24f81ec
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-10-04 18:09:41 +0200

    implements component mini-mapa dynamically (lesson 324 course angular)

--- src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, AfterViewInit, Input, ViewChild, ElementRef} from '@angular/core';
+import * as mapboxgl from "mapbox-gl";
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
   styles: [
+    `
+      div{
+        width: 100%;
+        height: 100px;
+        margin: 0;
+      }
+    `
   ]
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
+
+  @Input() lngLat: [number, number] = [0,0];
+  @ViewChild('mapa') divMapa!: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 10,
+      interactive: false
+    });
 
+    new mapboxgl.Marker()
+      .setLngLat( this.lngLat )
+      .addTo( map );
+  }
 }
