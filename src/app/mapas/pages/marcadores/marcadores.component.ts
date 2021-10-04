2be31d8d58c7b345c00edced959446dffbb5124e
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-10-04 15:36:22 +0200

    implements markers FlyTo method mapbox (lesson 319 course angular)

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -72,7 +72,17 @@ export class MarcadoresComponent implements AfterViewInit {
     } );
   }
 
-  irMarcador(){
-    // this.mapa.flyTo()
+  irMarcador(marker: mapboxgl.Marker){
+    this.mapa.flyTo({
+      center: marker.getLngLat()
+    });
+  }
+
+  guardarMarcadoresLocalStorage(){
+
+  }
+
+  leerLocalStorage(){
+
   }
 }
